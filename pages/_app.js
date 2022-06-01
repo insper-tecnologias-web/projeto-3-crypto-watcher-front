d9b15746fac42878dc923d64208879ec230c3960
Ricardo Ribeiro Rodrigues <riywar@gmail.com>
2022-06-01 17:53:13 -0300

    Adding SSRProvider to app + changing some css styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css';
+import SSRProvider from 'react-bootstrap/SSRProvider';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <SSRProvider><Component {...pageProps} /></SSRProvider>
 }
 
 export default MyApp
