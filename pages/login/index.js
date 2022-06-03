c3ea631f9098053d40513c86c1d788c8cc979d41
Ricardo Ribeiro Rodrigues <riywar@gmail.com>
2022-06-02 21:47:57 -0300

    Starting the login page

---
+++ pages/login/index.js
@@ -0,0 +1,46 @@
+import styles from '../../styles/LoginPage.module.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+export default function Login() {
+    return (
+        <div className={styles.container}>
+            <h1 style={{ fontWeight: 'bold', marginTop: '1rem', color: 'rgba(255, 255, 255, 0.9)' }}>
+                Please login to acess all the features.
+            </h1>
+            <div className={styles.formPaper}>
+                <h2 style={{ fontWeight: 'bold' }}>Login</h2>
+                <Form className={styles.formContainer}>
+                    <Form.Group className={`mb-3 ${styles.formGroup}`} controlId="formBasicEmail">
+                        <Form.Label>
+                            Email address
+                        </Form.Label>
+                        <InputGroup>
+                            <InputGroup.Text className={styles.iconContainer} id="basic-addon1">
+                                <svg style={{ color: 'rgba(255, 255, 255, 0.6)' }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                </svg>
+                            </InputGroup.Text>
+                            <Form.Control className={styles.inputStyle} type="email" placeholder="Enter email" />
+                        </InputGroup>
+                    </Form.Group>
+                    <Form.Group className={`mb-3 ${styles.formGroup}`} controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <InputGroup>
+                            <InputGroup.Text className={styles.iconContainer} id="basic-addon1">
+                                <svg style={{ color: 'rgba(255, 255, 255, 0.6)' }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+                                    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z" />
+                                </svg>
+                            </InputGroup.Text>
+                            <Form.Control className={styles.inputStyle} type="password" placeholder="Password" />
+                        </InputGroup>
+                    </Form.Group>
+                    <Button style={{ marginTop: '1rem' }} variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+        </div >
+    );
+}
