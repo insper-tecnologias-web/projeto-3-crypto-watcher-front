6e3be04cff5df8ef8795468d0ff882b35a1eb91d
Ricardo Ribeiro Rodrigues <riywar@gmail.com>
2022-06-07 08:53:46 -0300

    Fixed the all compilation errors

--- components/appbar.js
+++ components/appbar.js
@@ -6,21 +6,22 @@ import { useRouter } from "next/router";
 export default function AppBar() {
     const router = useRouter();
     const user = {
-        id:1,
-        email:"pedro@gmail.com",
+        id: 1,
+        email: "pedro@gmail.com",
     };
-  
+
     return (
         <div className={styles.AppBar}  >
             <div className={styles.AppBar_logo} onClick={() => router.push('/')}>
                 <Image src="/logo.png" alt="Logo" width={40} height={40} />
                 <h1 className={styles.AppBar_title}>CryptoWatchers</h1>
             </div>
-            <div className={styles.AppBar_wallet} onClick={{() => router.push({
-                        pathname:'/portfolio',
-                        query: user})}}>
+            <div className={styles.AppBar_wallet} onClick={() => router.push({
+                pathname: '/portfolio',
+                query: user
+            })}>
                 <Image src="/carteira.png" alt="Wallet" width={50} height={50} />
             </div>
-        </div>
+        </div >
     );
 }
