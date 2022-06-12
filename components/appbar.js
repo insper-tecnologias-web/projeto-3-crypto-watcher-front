02a4d169b3e928d937ec743fce7b2719d2eb130d
Ricardo Ribeiro Rodrigues <riywar@gmail.com>
2022-06-12 17:29:13 -0300

    Fixing appbar.js

--- components/appbar.js
+++ components/appbar.js
@@ -6,20 +6,21 @@ import { useRouter } from "next/router";
 export default function AppBar() {
     const router = useRouter();
     const user = {
-        id:1,
-        email:"pedro@gmail.com",
+        id: 1,
+        email: "pedro@gmail.com",
     };
-  
+
     return (
         <div className={styles.AppBar}  >
             <div className={styles.AppBar_logo} onClick={() => router.push('/')}>
                 <Image src="/logo.png" alt="Logo" width={40} height={40} />
                 <h1 className={styles.AppBar_title}>CryptoWatchers</h1>
             </div>
-            <div className={styles.AppBar_wallet} onClick={{() => router.push({
-                        pathname:'/portfolio',
-                        query: user})}}>
-                <Image src="/carteira.png" alt="Wallet" width={50} height={50} />
+            <div className={styles.AppBar_wallet} onClick={() => router.push({
+                pathname: '/portfolio',
+                query: { 'user': JSON.stringify(user) },
+            })}>
+                {router.pathname !== '/portfolio' && <Image src="/carteira.png" alt="Wallet" width={50} height={50} />}
             </div>
         </div>
     );
