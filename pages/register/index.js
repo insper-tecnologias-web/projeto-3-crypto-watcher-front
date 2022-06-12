c4843d3fbeeea9c365a7ab1462075182c6254fbb
guilherme fontana <guifl2001@hotmail.com>
2022-06-12 18:15:59 -0300

    register"

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,20 +1,60 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-import Link from 'next/link';
+import axios from 'axios';
 import { useState } from 'react';
 import styles from '../../styles/RegisterPage.module.css';
 
-export default function Login() {
+export default function Register() {
     const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     const handleEmailChange = (event) => {
         setEmail(event.target.value);
     };
 
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
     const handlePasswordChange = (event) => {
         setPassword(event.target.value);
+    };
+
+    const validateEmail = (email) => {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log(`Email: ${email}`);
+        console.log(`Username: ${username}`);
+        console.log(`Password: ${password}`);
+        if (validateEmail(email) === false) {
+            alert('Please enter a valid email');
+            return;
+        }
+        if (!username || !password || !email) {
+            alert('Please fill in all fields');
+            return;
+        }
+        const url = 'https://cryptic-bastion-47088.herokuapp.com/api/register/';
+        const data = {
+            username: username,
+            email: email,
+            password: password,
+        };
+        console.log(data);
+        await axios.post(url, data).then((response) => {
+            window.sessionStorage.setItem('userToken', response.data.token);
+            router.push("/");
+        }).catch((error) => {
+            if (error.response.status === 403) {
+                alert("Invalid username or password");
+            }
+        });
     };
 
 
@@ -34,8 +74,8 @@ export default function Login() {
                                     <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
                                 </svg>
                             </InputGroup.Text>
-                            <Form.Control onChange={handleEmailChange}
-                                className={styles.inputStyle} type="user" placeholder="Enter your username" />
+                            <Form.Control onChange={handleUsernameChange}
+                                className={styles.inputStyLoginle} type="user" placeholder="Enter your username" />
                         </InputGroup>
                     </Form.Group>
 
@@ -65,7 +105,7 @@ export default function Login() {
                                 className={styles.inputStyle} type="password" placeholder="Password" />
                         </InputGroup>
                     </Form.Group>
-                    <Button style={{ marginTop: '1rem' }} variant="primary" type="submit">
+                    <Button onClick={handleSubmit}  style={{ marginTop: '1rem' }} variant="primary" type="submit">
                         Register
                     </Button>
                 </Form>
