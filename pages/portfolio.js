9a232f570d812450690aa57bdb9b4e4de2e1e213
Ricardo Ribeiro Rodrigues <riywar@gmail.com>
2022-06-13 19:33:19 -0300

    Finishing the portfolio screen

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -9,15 +9,16 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import styles from '../styles/portfolio.module.css';
 
-export default function Userportfolio({allCryptos}) {
+export default function Userportfolio({ allCryptos }) {
 	const router = useRouter();
 	const allCoins = allCryptos.data;
 	const [userLog, setUserLog] = useState(false);
-	const [buyingprice,setBuyingprice] = useState(0);
-	const [lista,setLista] = useState([]);
-	const [name,setName] = useState("");
-	const [quantity,setQuantity] = useState(0);
-	const [notes,setNotes] = useState("");
+	const [buyingprice, setBuyingprice] = useState(0);
+	const [lista, setLista] = useState([]);
+	const [name, setName] = useState("");
+	const [quantity, setQuantity] = useState(0);
+	const [notes, setNotes] = useState("");
+	const [userName, setUserName] = useState("");
 
 
 	let allCoinsbetter = {}
@@ -25,35 +26,38 @@ export default function Userportfolio({allCryptos}) {
 		let name = a.id;
 		allCoinsbetter[name] = a
 	}
+	const integrateBackAndAPI = (response) => {
+		let u = [];
+		for (let moeda of response.data) {
+			if (Object.keys(allCoinsbetter).indexOf(moeda.crypto_id) !== -1) {
+				u.push({
+					moedauser: moeda,
+					dadosapi: allCoinsbetter[moeda.crypto_id],
+				})
+			}
+		}
+		return u;
+	}
 
-	useEffect(()=>{
+	useEffect(() => {
 		setUserLog(window.sessionStorage.getItem('userToken'));
-	},[router.query.slug, userLog])
+		setUserName(window.sessionStorage.getItem('username'));
+	}, [router.query.slug, userLog])
 
-	useEffect(()=>{
-		if (userLog){
-			axios.get('https://cryptic-bastion-47088.herokuapp.com/api/cryptos/',{
-				headers:{Authorization:"Token "+userLog}
-			}).then(response=>{
-				console.log(response)
-				let u = []
-				for (let moeda of response.data){
-					if (Object.keys(allCoinsbetter).indexOf(moeda.crypto_id)!==-1){
-						u.push({
-							moedauser:moeda,
-							dadosapi:allCoinsbetter[moeda.crypto_id],
-						})
-					}
-				}
-				setLista(u)
+	useEffect(() => {
+		if (userLog) {
+			axios.get('https://cryptic-bastion-47088.herokuapp.com/api/cryptos/', {
+				headers: { Authorization: "Token " + userLog }
+			}).then(response => {
+				let u = integrateBackAndAPI(response);
+				setLista(u);
 			})
 		}
-	},[userLog]);
-		
+	}, [userLog]);
+
 
 	const handleNameChange = (event) => {
 		let cryname = event.target.value;
-		console.log(cryname)
 		setName(cryname);
 	};
 
@@ -61,36 +65,42 @@ export default function Userportfolio({allCryptos}) {
 		setQuantity(event.target.value);
 	};
 
-	const handleNotesChange=(event)=>{
+	const handleNotesChange = (event) => {
 		setNotes(event.target.value);
 	}
-	
+
 	const handleBuyingpriceChange = (event) => {
 		setBuyingprice(event.target.value);
 	};
-		
+
 	const handleSubmit = async (event) => {
 		event.preventDefault();
 		if (!name || !quantity || !notes) {
-				alert('Please fill in all fields');
-				return;
+			alert('Please fill in all fields');
+			return;
+		}
+		// Validate the crypto name
+		if (!Object.keys(allCoinsbetter).includes(name.toLowerCase())) {
+			alert('Invalid crypto name');
+			return;
 		}
 		const url = 'https://cryptic-bastion-47088.herokuapp.com/api/cryptos/';
 		const data = {
-			crypto_id: name,
+			crypto_id: name.toLowerCase(),
 			quantity: quantity,
-			buying_price : buyingprice,
+			buying_price: buyingprice,
 			notes: notes,
 		};
 		const headers = {
-			'Content-Type' : 'application/json',
-			Authorization:"Token "+ userLog
+			'Content-Type': 'application/json',
+			Authorization: "Token " + userLog
 		}
-		await axios.post(url, data, {headers:headers}).then((response) => {
-			router.push("/");
+		await axios.post(url, data, { headers: headers }).then((response) => {
+			let u = integrateBackAndAPI(response);
+			setLista(u);
 		}).catch((error) => {
 			if (error.response.status === 403) {
-				alert("Invalid");
+				alert("Invalid crypto");
 			}
 		});
 	};
@@ -98,24 +108,25 @@ export default function Userportfolio({allCryptos}) {
 	return (
 		<div>
 			<AppBar></AppBar>
-			<h1> cryptocurrency portfolio</h1>
+			{userName && <h1 style={{ display: 'block', margin: 'auto', width: '30%' }}>{userName}{"'s"} portfolio</h1>}
 			<div className={styles.container}>
-				<Table striped bordered hover variant="dark">
+				<Table style={{ margin: '1rem 0.5rem 2rem 0.5rem' }} striped bordered hover variant="dark">
 					<thead>
-							<tr>
-								<th>Ranking</th>
-								<th>Ticker</th>
-								<th>Name</th>
-								<th>Quantity</th>
-								<th>price Usd</th>
-								<th>buying_price</th>
-								<th>changePercent24Hr</th>
-								<th>Details</th>
-								<th>#</th>
-							</tr>
+						<tr>
+							<th>Ranking</th>
+							<th>Ticker</th>
+							<th>Name</th>
+							<th>Quantity</th>
+							<th>Price now(Usd)</th>
+							<th>Buying price</th>
+							<th>Change in 24h(%)</th>
+							<th>Profit</th>
+							<th>Details</th>
+							<th>#</th>
+						</tr>
 					</thead>
-					<tbody> 
-						{lista.map((coin)=>(
+					<tbody>
+						{lista.map((coin) => (
 							<tr key={coin.moedauser.id}>
 								<td>{coin.dadosapi.rank}</td>
 								<td>{coin.dadosapi.symbol}</td>
@@ -124,24 +135,27 @@ export default function Userportfolio({allCryptos}) {
 								<td>{coin.dadosapi.priceUsd}</td>
 								<td>{coin.moedauser.buying_price}</td>
 								<td>{parseFloat(coin.dadosapi.changePercent24Hr).toFixed(2)} %</td>
+								<td>{coin.dadosapi.priceUsd * coin.moedauser.quantity - coin.moedauser.buying_price * coin.moedauser.quantity}</td>
 								<td>{coin.moedauser.notes}</td>
 								<td>
-									<Link href={`crypto/${coin.moedauser.crypto_id}`}>
+									<Link href={`/crypto/${coin.moedauser.crypto_id}`}>
 										<Button variant="outline-light">See details</Button>
 									</Link>
 								</td>
-						</tr>
+							</tr>
 						))}
 					</tbody>
 				</Table>
-				
+
 				<Form className={styles.formContainer}>
+					<h2>Add a crypto to your portfolio</h2>
 					<Form.Group>
 						<Form.Label>
 							Cryptocurrency name:
 						</Form.Label>
 						<InputGroup>
-							<Form.Control onChange={handleNameChange} type="text" placeholder="Enter cryptocurrency name" />
+							<Form.Control className={styles.inputStyle}
+								onChange={handleNameChange} type="text" placeholder="Enter cryptocurrency name" />
 						</InputGroup>
 					</Form.Group>
 					<Form.Group>
@@ -149,7 +163,8 @@ export default function Userportfolio({allCryptos}) {
 							Quantity:
 						</Form.Label>
 						<InputGroup>
-							<Form.Control onChange={handleQuantityChange} type="number" placeholder="Enter quantity" />
+							<Form.Control className={styles.inputStyle}
+								onChange={handleQuantityChange} type="number" placeholder="Enter quantity" />
 						</InputGroup>
 					</Form.Group>
 					<Form.Group>
@@ -157,7 +172,8 @@ export default function Userportfolio({allCryptos}) {
 							Buying price:
 						</Form.Label>
 						<InputGroup>
-							<Form.Control onChange={handleBuyingpriceChange} type="number" placeholder="Enter buying price" />
+							<Form.Control className={styles.inputStyle}
+								onChange={handleBuyingpriceChange} type="number" placeholder="Enter buying price" />
 						</InputGroup>
 					</Form.Group>
 					<Form.Group>
@@ -165,7 +181,8 @@ export default function Userportfolio({allCryptos}) {
 							Notes:
 						</Form.Label>
 						<InputGroup>
-							<Form.Control onChange={handleNotesChange} type="text" placeholder="Enter notes" />
+							<Form.Control className={styles.inputStyle}
+								onChange={handleNotesChange} type="text" placeholder="Enter notes" />
 						</InputGroup>
 					</Form.Group>
 					<Button onClick={handleSubmit} style={{ marginTop: '1rem' }} variant="primary" type="submit">
@@ -179,8 +196,8 @@ export default function Userportfolio({allCryptos}) {
 
 export async function getServerSideProps(context) {
 	const { query, params, req } = context;
-	
-	const allCryptos = await fetch('https://api.coincap.io/v2/assets/', { Authorization: "Bearer " + process.env.API_KEY }).then((response)=>response.json())
+
+	const allCryptos = await fetch('https://api.coincap.io/v2/assets/', { Authorization: "Bearer " + process.env.API_KEY }).then((response) => response.json())
 
 	return {
 		props: { allCryptos }
